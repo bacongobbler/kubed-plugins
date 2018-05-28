91daf6a6db5df8e7bf5b285264ec4617d233824a
Matthew Fisher <matt.fisher@microsoft.com>
2018-05-28 11:25:54 -0700

    generator-controller 1.0.0

--- Plugins/generator-controller.lua
+++ Plugins/generator-controller.lua
@@ -1,5 +1,5 @@
 local name = "generator-controller"
-local version = "0.1.0"
+local version = "1.0.0"
 
 plugin = {
     name = name,
@@ -12,21 +12,21 @@ plugin = {
             os = "darwin",
             arch = "amd64",
             url = "https://bacongobbler.blob.core.windows.net/stuff/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "257e58af86095869a5eeff7ef9c1b256fdc2c2cd00bd5cf561f76cb677c54cfe",
+            sha256 = "696e6fa999f23e4381686e531d41165b6b723bbcaebbcc7047c674760cec8905",
             path = "darwin-amd64/" .. name,
         },
         {
             os = "linux",
             arch = "amd64",
             url = "https://bacongobbler.blob.core.windows.net/stuff/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "4609931fe17800d34d62c7d113f8e679e47d48444982606f9fc26504dca45479",
+            sha256 = "a902dd4e717c723a9f475e9c1e77e3cd3014189e9d795b96fbd7c237b6e4bc8d",
             path = "linux-amd64/" .. name,
         },
         {
             os = "windows",
             arch = "amd64",
             url = "https://bacongobbler.blob.core.windows.net/stuff/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "8a889f0598a0f6d4ddea8ef2758fe9c154806ac40b2c5d3185c833829768ee0a",
+            sha256 = "5c22b2c02412aaa44ab52e0cff1287be9c55e31745714401f1246f6bf877a9e8",
             path = "windows-amd64/" .. name .. ".exe",
         }
     }
